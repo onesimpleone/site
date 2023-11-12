27ad122da0990be6ed7cbe6f94ad8f7ec74a7a61
thanksyouall <tech.development.mail@gmail.com>
2023-11-12 11:16:32 +0500

    Remove old

--- src/utils/seo/seo.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Позволяет настроить заголовки и любые другие данные
- * важные для seo. Просто вызываем
- * компонент на нашей странице и передаём сюда значения.
- * 
- * КАК НАСТРОИТЬ:
- * https://www.gatsbyjs.com/docs/add-page-metadata/
- * и https://www.gatsbyjs.com/docs/add-seo-component/
- */
