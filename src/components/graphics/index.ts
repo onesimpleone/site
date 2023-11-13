f0d0f04811f1730755404e20e6b8ce0e84431155
bouncycatt <anna.onesimple@gmail.com>
2023-11-13 12:49:16 +0500

    Add Meta Badge

---
+++ src/components/graphics/index.ts
@@ -0,0 +1,4 @@
+export { Logo } from './Logo/Logo'
+export { Bubble } from './Bubble/Bubble'
+export { DownloadBadge } from './DownloadBadge/DownloadBadge'
+export { MetaBadge } from './MetaBadge/MetaBadge'
