a7cabc5aded0c2f2a94cb7d8b5dc362aedd3c797
bouncycatt <anna.onesimple@gmail.com>
2023-11-13 12:46:08 +0500

    Create Secondary Button

---
+++ src/components/buttons/SecondaryButton/SecondaryButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { Link } from 'gatsby'
+
+import * as styles from './SecondaryButton.module.scss'
+import * as presets from 'styles/presets.module.scss'
+
+interface IProps {
+  /** Button title */
+  title: string
+
+  /** Container style */
+  className?: string
+
+  /** Path */
+  path?: string
+}
+
+export const SecondaryButton = ({ className, path = '/', title }: IProps) => {
+  const styleSet = {
+    navLink: `${presets.secondaryButton} ${styles.navLink}`,
+    navLinkActive: `${presets.secondaryButton} ${styles.navLink}`,
+  }
+
+  return (
+    <Link
+      activeClassName={styleSet.navLinkActive}
+      className={[styleSet.navLink, className].join(' ')}
+      to={path}
+    >
+      {title}
+    </Link>
+  )
+}
