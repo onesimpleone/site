e5a9dbbb97497df2bb753479d06601c6d299c588
bouncycatt <anna.onesimple@gmail.com>
2023-11-13 12:45:57 +0500

    Create Primary Button

---
+++ src/components/buttons/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { Link } from 'gatsby'
+
+import * as styles from './PrimaryButton.module.scss'
+import * as presets from 'styles/presets.module.scss'
+
+interface IProps {
+  /** Button title */
+  title: string
+
+  /** Container style */
+  className?: string
+
+  /** Path */
+  path?: string
+}
+
+export const PrimaryButton = ({ className, path = '/', title }: IProps) => {
+  const styleSet = {
+    navLink: `${presets.primaryButton} ${styles.navLink}`,
+    navLinkActive: `${presets.primaryButton} ${styles.navLink}`,
+  }
+
+  return (
+    <Link
+      activeClassName={styleSet.navLinkActive}
+      className={[styleSet.navLink, className].join(' ')}
+      to={path}
+    >
+      {title}
+    </Link>
+  )
+}
