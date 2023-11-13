b459a672df47bc9325347e560405a56a34656b05
bouncycatt <anna.onesimple@gmail.com>
2023-11-13 18:33:48 +0500

    Remove unnecessary code

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -19,12 +19,6 @@ const config: GatsbyConfig = {
     // "gatsby-plugin-google-gtag",
     'gatsby-plugin-image',
     'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        icon: 'src/images/icon.png',
-      },
-    },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
@@ -49,14 +43,8 @@ const config: GatsbyConfig = {
         start_url: '/',
         background_color: '#0D0D0D',
         theme_color: '#0D0D0D',
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
+        icon: 'src/images/icon.png',
       },
     },
   ],
