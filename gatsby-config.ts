26742362e62b387aa2f299305c6924c09dc7037f
bouncycatt <anna.onesimple@gmail.com>
2023-11-13 13:59:54 +0500

    Add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -41,6 +41,24 @@ const config: GatsbyConfig = {
         siteUrl: new URL(siteAddress).href.slice(0, -1),
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'OneSimple',
+        short_name: 'OneSimple',
+        start_url: '/',
+        background_color: '#0D0D0D',
+        theme_color: '#0D0D0D',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
 
