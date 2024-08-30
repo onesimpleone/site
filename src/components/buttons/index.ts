2d3e9d2475dcf76d1bc0471e62f7696293033cea
bouncycatt <anna.onesimple@gmail.com>
2024-08-30 17:51:14 +0700

    Create CTA Button

--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,2 +1,4 @@
 export { PrimaryButton } from './PrimaryButton/PrimaryButton'
 export { SecondaryButton } from './SecondaryButton/SecondaryButton'
+export { DownloadButtons } from './DownloadButtons/DownloadButtons'
+export { CtaButton } from './CtaButton/CtaButton'
